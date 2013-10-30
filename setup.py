0607ab396a0a15aeaf3f508b144287a122607b2a
Chris McBride <chris@vistarmedia.com>
2013-10-30 17:14:19 -0400

    get ird of requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
   name='ddmiddleware',
   version='0.1',
-  install_requires=reqs,
+  install_requires=['dogapi==1.6.0'],
   packages=['ddmiddleware'])
